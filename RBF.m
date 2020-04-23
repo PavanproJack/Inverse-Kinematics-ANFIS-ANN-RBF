064ee05b9f11739d7754adaa0fdb0172decc6a9d
PavanKumar Kavvuri <PavanproJack@github.com>
2020-04-23 17:27:21 +0100

    Fine tuned ANN and RadBasis, ANFIS networks

--- RBF.m
+++ RBF.m
@@ -1,32 +1,44 @@
 
-l1 = 10; % length of first arm
-l2 = 7; % length of second arm
-l3 = 5; % length of third arm
+clc, clear 
 
-%{
+%% Data Generation
+% Defining Link Lengths here..
+linkLength_1 = 10;                         % length of first arm
+linkLength_2 = 7;                           % length of second arm
+linkLength_3 = 5;                           % length of third arm 
+
 %%traning data
-theta1 = rand(1,12)*90; % all possible theta1 values
-theta2 = rand(1,12)*90; % all possible theta2 values
-theta3 = rand(1,12)*90; % all possible theta3 values
-%}
-theta1= 0 : 3.5 : 90;
-theta2= 0 : 3.5 : 90;
-theta3= 0 : 3.5 : 90;
-
-
+%theta1 = rand(1,12)*90; % all possible theta1 values
+%theta2 = rand(1,12)*90; % all possible theta2 values
+%theta3 = rand(1,12)*90; % all possible theta3 values
 % generate a grid of theta1 and theta2 and theta3 values
-[THETA1, THETA2,THETA3] = ndgrid(theta1, theta2, theta3); 
-% compute x coordinates
-X = l1 * cos(THETA1*pi/180) + l2 * cos(THETA1*pi/180 + THETA2*pi/180) + l3*cos(THETA1*pi/180+THETA2*pi/180+THETA3*pi/180); 
-  % compute y coordinates
-Y = l1 * sin(THETA1*pi/180) + l2 * sin(THETA1*pi/180 + THETA2*pi/180) + l3*sin(THETA1*pi/180+THETA2*pi/180+THETA3*pi/180);
+
+theta_1= 0 : 3.5 : 90;
+theta_2= 0 : 3.5 : 90;
+theta_3= 0 : 3.5 : 90;
+
+% Replicating the grid vectors theta_1, theta_2, theta_3 to produce an n-dimensional full grid
+[THETA1, THETA2,THETA3] = ndgrid(theta_1, theta_2, theta_3); 
+
+%%Forward Kinematics... Let 'X', 'Y', and 'phi' be the end effector configuration. Geometrically, they are caluculated as ...
+
+X = linkLength_1 * cos(THETA1*pi/180) + ...
+    linkLength_2 * cos(THETA1*pi/180 + THETA2*pi/180) + ...
+    linkLength_3*cos(THETA1*pi/180+THETA2*pi/180+THETA3*pi/180); 
+ 
+Y = linkLength_1 * sin(THETA1*pi/180) + ...
+    linkLength_2 * sin(THETA1*pi/180 + THETA2*pi/180) + ...
+    linkLength_3*sin(THETA1*pi/180+THETA2*pi/180+THETA3*pi/180);
+
 phi = THETA1 + THETA2 + THETA3;
-% create training dataset
+
 data = [X(:) Y(:) phi(:) THETA1(:) THETA2(:) THETA3(:)]; 
 
+% Randomly shuffle the data
 data_ = data(  randperm( size(data, 1) ),   :  );
 
-%Data Splitting
+%% Data Partitioning into Train, Test and Validation sets. 
+
 train_partition_1 = data_(1:round( size(data_,1)*5/7),1:4);  
 train_partition_2 = data_(1:round(size(data_,1)*5/7),[1,2,3,5]);
 train_partition_3 = data_(1:round(size(data_,1)*5/7),[1,2,3,6]);  
@@ -38,72 +50,67 @@ check_partition_3 = data_(round(size(data_,1)*5/7)+1:round(size(data_,1)*6/7),[1
 test_partition_1 = data_(round(size(data_,1)*6/7)+1:size(data_,1),1:4);
 test_partition_2 = data_(round(size(data_,1)*6/7)+1:size(data_,1),[1,2,3,5]);
 test_partition_3 = data_(round(size(data_,1)*6/7)+1:size(data_,1),[1,2,3,6]);
-%
 
-hiddenSizes = 10;
 
+%%
+
+hiddenSizes = 50;
+trainingFunction = 'trainbr';
 %net1=newrb(train_partition_1(:,1:3)',train_partition_1(:,4)',0.8,5,300,8);
-net1= fitnet(hiddenSizes);
+net1= fitnet(hiddenSizes, trainingFunction);
 net1.layers{1}.transferFcn = 'radbas';
-tic
 net1_model = train(net1,train_partition_1(:,1:3)',train_partition_1(:,4)');
-toc
-thetar1=sim(net1_model, test_partition_1(:,1:3)');
 
+predicted_Theta_1=sim(net1_model, test_partition_1(:,1:3)');
+
+perf_1 = perform(net1_model, predicted_Theta_1 , test_partition_1(:,4)');
+disp(perf_1);
+
+%%%%%%%%
 %net2=newrb(train_partition_2(:,1:3)',train_partition_2(:,4)',0.8,5,300,8);
-net2= fitnet(hiddenSizes);
+net2= fitnet(hiddenSizes, trainingFunction);
 net2.layers{1}.transferFcn = 'radbas';
-tic
 net2_model = train(net2,train_partition_2(:,1:3)',train_partition_2(:,4)');
-toc
-thetar2=sim(net2_model, test_partition_2(:,1:3)');
+
+predicted_Theta_2=sim(net2_model, test_partition_2(:,1:3)');
+
+perf_2 = perform(net2_model, predicted_Theta_2 , test_partition_2(:,4)');
+disp(perf_2);
+%%%%%%%
 
 %net3=newrb(train_partition_3(:,1:3)',train_partition_3(:,4)',0.8,5,300,8);
-net3= fitnet(hiddenSizes);
+net3= fitnet(hiddenSizes, trainingFunction);
 net3.layers{1}.transferFcn = 'radbas';
-tic
 net3_model = train(net3,train_partition_3(:,1:3)',train_partition_3(:,4)');
-toc
-thetar3=sim(net3_model, test_partition_3(:,1:3)');
 
-thetar1_diff=test_partition_1(:,4)-thetar1(:);
-thetar2_diff=test_partition_2(:,4)-thetar2(:);
-thetar3_diff=test_partition_3(:,4)-thetar3(:);
+predicted_Theta_3=sim(net3_model, test_partition_3(:,1:3)');
+
+perf_3 = perform(net3_model, predicted_Theta_3 , test_partition_3(:,4)');
+disp(perf_3);
+
+thetaDifference_1 = test_partition_1(:,4)' - predicted_Theta_1;
+thetaDifference_2 = test_partition_2(:,4)' - predicted_Theta_2;
+thetaDifference_3 = test_partition_3(:,4)' - predicted_Theta_3;
+
+
+%% Plot the Deviation from the targets.
 
 
 figure()
 subplot(3,1,1);
-plot(thetar1_diff);
-ylabel('theta1 error')
-title('Desired theta1 - Predicted theta1(degree)')
+plot(thetaDifference_1);
+ylabel(['Theta 1 Error']);
+title('Desired theta1 - Predicted theta1 (in degrees)');
 
 subplot(3,1,2);
-plot(thetar2_diff);
-ylabel('theta2 error')
-title('Desired theta2 - Predicted theta2(degree)')
+plot(thetaDifference_2);
+ylabel(['Theta 3 Error']);
+title('Desired theta2 - Predicted theta2 (in degrees)');
 
 subplot(3,1,3);
-plot(thetar3_diff);
-ylabel('theta3 error')
-title('Desired theta3 - Predicted theta3(degree)')
-
-
-%{
-Xr = l1 * cos(thetar1*pi/180) + l2 * cos(thetar1*pi/180 + thetar2*pi/180) 
-+l3*cos(thetar1*pi/180 + thetar2*pi/180 + thetar3*pi/180); 
-Yr = l1 * sin(thetar1*pi/180) + l2 * sin(thetar1*pi/180 + thetar2*pi/180) 
-+l3*sin(thetar1*pi/180 + thetar2*pi/180 + thetar3*pi/180);
-
-xr_diff=test_partition_1(:,1)-Xr(:);
-yr_diff=test_partition_1(:,2)-Yr(:);
-%error=sum(sqrt(xr_diff.^2+yr_diff.^2))/length(xr_diff);
-xyrMSE=sum(xr_diff.^2+yr_diff.^2)/length(xr_diff);
-xyrMAE=sum(abs(xr_diff)+abs(yr_diff))/length(xr_diff);
-%}
-
-
-%figure()
-%quiver(test_partition_1(:,1),test_partition_1(:,2),xr_diff,yr_diff)
+plot(thetaDifference_3);
+ylabel(['Theta 3 Error']);
+title('Desired theta3 - Predicted theta3 (in degrees)');
 
 
 
--- TakeInputs.m
+++ TakeInputs.m
@@ -1,36 +1,52 @@
 
-
 clc, clear 
 
-l1 = 10; 
-l2 = 7;
-l3 = 5; 
+%% Data Generation
+% Defining Link Lengths here..
+linkLength_1 = 10;                         % length of first arm
+linkLength_2 = 7;                           % length of second arm
+linkLength_3 = 5;                           % length of third arm 
 
-%{
-theta1 = rand(1,12)*90;
-theta2 = rand(1,12)*90; 
-theta3 = rand(1,12)*90; 
-%}
+%%traning data
+%theta1 = rand(1,12)*90; % all possible theta1 values
+%theta2 = rand(1,12)*90; % all possible theta2 values
+%theta3 = rand(1,12)*90; % all possible theta3 values
+% generate a grid of theta1 and theta2 and theta3 values
 
-theta1= 0 : 3.5 : 90;
-theta2= 0 : 3.5 : 90;
-theta3= 0 : 3.5 : 90;
+theta_1= 0 : 3.5 : 90;
+theta_2= 0 : 3.5 : 90;
+theta_3= 0 : 3.5 : 90;
 
+% Replicating the grid vectors theta_1, theta_2, theta_3 to produce an n-dimensional full grid
+[THETA1, THETA2,THETA3] = ndgrid(theta_1, theta_2, theta_3); 
 
-[THETA1, THETA2,THETA3] = ndgrid(theta1, theta2, theta3); 
+%%Forward Kinematics... Let 'X', 'Y', and 'phi' be the end effector configuration. Geometrically, they are caluculated as ...
 
-X = l1 * cos(THETA1*pi/180) + l2 * cos(THETA1*pi/180 + THETA2*pi/180) + l3*cos(THETA1*pi/180+THETA2*pi/180+THETA3*pi/180); 
+X = linkLength_1 * cos(THETA1*pi/180) + ...
+    linkLength_2 * cos(THETA1*pi/180 + THETA2*pi/180) + ...
+    linkLength_3*cos(THETA1*pi/180+THETA2*pi/180+THETA3*pi/180); 
  
-Y = l1 * sin(THETA1*pi/180) + l2 * sin(THETA1*pi/180 + THETA2*pi/180) + l3*sin(THETA1*pi/180+THETA2*pi/180+THETA3*pi/180);
+Y = linkLength_1 * sin(THETA1*pi/180) + ...
+    linkLength_2 * sin(THETA1*pi/180 + THETA2*pi/180) + ...
+    linkLength_3*sin(THETA1*pi/180+THETA2*pi/180+THETA3*pi/180);
+
 phi = THETA1 + THETA2 + THETA3;
+
+%Plot the Workspace
+%{
+plot (double(X(:)), double(Y(:)) ,'.' , 'Color', [255, 153, 51] / 255)
+ylabel('Y');
+xlabel('X');
+title('X  Y Coordinates generated for all joint angle combinations using Forward Kinematics');
+%}
 
 data = [X(:) Y(:) phi(:) THETA1(:) THETA2(:) THETA3(:)]; 
 
+% Randomly shuffle the data
 data_ = data(  randperm( size(data, 1) ),   :  );
 
-%plot (double(X(:)), double(Y(:)) ,'.' , 'Color', [255, 153, 51] / 255)
+%% Data Partitioning into Train, Test and Validation sets. 
 
-%Data Splitting
 train_partition_1 = data_(1:round( size(data_,1)*5/7),1:4);  
 train_partition_2 = data_(1:round(size(data_,1)*5/7),[1,2,3,5]);
 train_partition_3 = data_(1:round(size(data_,1)*5/7),[1,2,3,6]);  
@@ -42,76 +58,78 @@ check_partition_3 = data_(round(size(data_,1)*5/7)+1:round(size(data_,1)*6/7),[1
 test_partition_1 = data_(round(size(data_,1)*6/7)+1:size(data_,1),1:4);
 test_partition_2 = data_(round(size(data_,1)*6/7)+1:size(data_,1),[1,2,3,5]);
 test_partition_3 = data_(round(size(data_,1)*6/7)+1:size(data_,1),[1,2,3,6]);
-%
+
+%%
 
 %surf (double(train_partition_1(:, 1)), double(train_partition_1(:, 2)), double(train_partition_1(:, 4)) )
 %colorbar
 
-%clusteringType = input('Input the type of clustering :   ');
-clusteringType = 1;
+clusteringType = input('Input the type of clustering :   ');
+%clusteringType = 1;
 %syms clusteringType 
 mF = ["gaussmf"  "gaussmf" "gaussmf"];
+
 %mf  = input('Input the type of MF :   ');
 
 switch clusteringType
-      case 1  % GridPartition
+    
+    case 1   %% GridPartition
           
           disp("Selected Clustering type : Grid Partition");
           
             genfisOpt_1 = genfisOptions('GridPartition');
-            genfisOpt_1.NumMembershipFunctions = [3 3 3];
+            genfisOpt_1.NumMembershipFunctions = [4 4 4];
             genfisOpt_1.InputMembershipFunctionType = mF;
             genfisObject_1=genfis(train_partition_1(:, 1:3), train_partition_1(:, 4), genfisOpt_1);
             
                 genfisOpt_2 = genfisOptions('GridPartition');
-                genfisOpt_2.NumMembershipFunctions = [3 3 3 ];
+                genfisOpt_2.NumMembershipFunctions = [4 4 4];
                 genfisOpt_2.InputMembershipFunctionType = mF;
                 genfisObject_2=genfis(train_partition_2(:, 1:3),train_partition_2(:, 4), genfisOpt_2);
 
                 genfisOpt_3 = genfisOptions('GridPartition');
-                genfisOpt_3.NumMembershipFunctions = [3 3 3];
+                genfisOpt_3.NumMembershipFunctions = [4 4 4];
                 genfisOpt_3.InputMembershipFunctionType = mF;
                 genfisObject_3=genfis(train_partition_3(:, 1:3),train_partition_3(:, 4), genfisOpt_3);
              
-                
-            [a1, b1, c1, d1] = anfisEval(train_partition_1, check_partition_1, test_partition_1, genfisObject_1, 1);
-            
-            [a2, b2, c2, d2] = anfisEval(train_partition_2, check_partition_2, test_partition_2, genfisObject_2, 2);
-            
-            [a3, b3, c3, d3] = anfisEval(train_partition_3, check_partition_3, test_partition_3, genfisObject_3, 3);
-            
-            
-            
-      case 2  % FCM Clustering
+               
+           
+      case 2  %% FCM Clustering 
           
             genfisOpt_1 = genfisOptions('FCMClustering' );
-            genfisObject_1=genfis(trndata1(:, 1:3),trndata1(:, 4), genfisOpt);
+            genfisObject_1=genfis(train_partition_1(:, 1:3),train_partition_1(:, 4), genfisOpt_1);
             
             genfisOpt_2 = genfisOptions('FCMClustering' );
-            genfisObject_2=genfis(trndata1(:, 1:3),trndata1(:, 4), genfisOpt);
+            genfisObject_2=genfis(train_partition_2(:, 1:3),train_partition_2(:, 4), genfisOpt_2);
             
             genfisOpt_3 = genfisOptions('FCMClustering' );
-            genfisObject_3=genfis(trndata1(:, 1:3),trndata1(:, 4), genfisOpt);
+            genfisObject_3=genfis(train_partition_3(:, 1:3),train_partition_3(:, 4), genfisOpt_3);
             
-            anfisOpt_1 = anfisOptions('InitialFIS',genfisObject_1);
-             outFis = anfis(trainData(:,1:4), anfisOpt_1);
-            
-            
-            
-      case 3  % Subtractive Clustering
+           
+      case 3 %%  % Subtractive Clustering
           
             genfisOpt_1 = genfisOptions('SubtractiveClustering', 'ClusterInfluenceRange',[0.5 0.25 0.3 0.4]); 
-            genfisObject_1=genfis(trndata1(:, 1:3),trndata1(:, 4), genfisOpt);
+            genfisObject_1=genfis(train_partition_1(:, 1:3),train_partition_1(:, 4), genfisOpt_1);
             
             genfisOpt_2 = genfisOptions('SubtractiveClustering', 'ClusterInfluenceRange',[0.5 0.25 0.3 0.4]); 
-            genfisObject_2=genfis(trndata1(:, 1:3),trndata1(:, 4), genfisOpt);
+            genfisObject_2=genfis(train_partition_2(:, 1:3),train_partition_2(:, 4), genfisOpt_2);
             
             genfisOpt_3 = genfisOptions('SubtractiveClustering', 'ClusterInfluenceRange',[0.5 0.25 0.3 0.4]); 
-            genfisObject_3 = genfis(trndata1(:, 1:3),trndata1(:, 4), genfisOpt);
+            genfisObject_3 = genfis(train_partition_3(:, 1:3),train_partition_3(:, 4), genfisOpt_3);
             
     otherwise 
             disp('Something went wrong')
 end
+
+
+
+           anfisEval(train_partition_1, check_partition_1, test_partition_1, genfisObject_1, 1);
+            
+           anfisEval(train_partition_2, check_partition_2, test_partition_2, genfisObject_2, 2);
+            
+           anfisEval(train_partition_3, check_partition_3, test_partition_3, genfisObject_3, 3);
+
+
 
 
 
